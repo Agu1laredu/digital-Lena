c186560492c5fa0ae97221b10aaab628ac6b8db5
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-06-23 18:51:03 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,64 @@ function App() {
           </div>
         </div>
       </div>
+      <section
+        style={{
+          width: "90%",
+          margin: " 15% auto",
+          textAlign: "center",
+          display: "grid",
+        }}
+      >
+        <h2 style={{ color: "#fec0e7", fontSize: "60px" }}>SOBRE MI</h2>
+        <b
+          style={{
+            textAlign: "justify",
+            width: "80%",
+            margin: "auto",
+            fontSize: "100%",
+          }}
+        >
+          Soy Malena Aguilar, COMUNNITY MANAGER certificada en CODERHOUSE,
+          extendi mis conocimientos en Platzi con los cursos de Estrategias de
+          Marketing en Redes Sociales , aun sigo realizando cursos en esta
+          plataforma para poder tener muchos más conocimientos incluso ademas de
+          los que tengo
+        </b>
+        <b
+          style={{
+            textAlign: "justify",
+            width: "80%",
+            margin: "auto",
+            fontSize: "100%",
+          }}
+        >
+          Me apasionan las redes sociuales y la creación de contenido par alas
+          mismas , es un mund infinito que me encanta explorar y ser parte de
+          este mundo digital con el Marketing
+        </b>
+        <button
+          style={{
+            backgroundColor: "#fec0e7",
+            fontSize: "100%",
+            border: "none",
+            margin: " 10% auto",
+            width: "200px",
+            padding: "15px",
+            borderRadius: "15px",
+          }}
+        >
+          <b
+            href=""
+            style={{
+              fontSize: "100%",
+              textDecoration: "none",
+              color: "#010101",
+            }}
+          >
+            INSTAGRAM
+          </b>
+        </button>
+      </section>
     </>
   );
 }
--- src/screen/SobreMi.jsx
+++ src/screen/SobreMi.jsx
@@ -1,12 +1,5 @@
-import NavBar from "../components/Menu/navbar";
-
 function SobreMi() {
-  return (
-    <>
-      <NavBar />
-      <h2>Sobre Mi</h2>
-    </>
-  );
+  return <h2>hola</h2>;
 }
 
 export default SobreMi;
